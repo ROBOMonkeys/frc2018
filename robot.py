2eaafc0a3e3ae2a2d7edecb822e92499cc185836
Brownman371 <castenada14@gmail.com>
2018-02-17 14:36:29 -0500

    autonomous movement

--- robot.py
+++ robot.py
@@ -4,6 +4,14 @@ from wpilib.interfaces.generichid import GenericHID
 
 class MyRobot(wpi.IterativeRobot):
     def robotInit(self):
+        self.auto_state = 0
+
+        self.sd = wpi.SmartDashboard()
+
+        self.sd.putBoolean("Autonomous Center", False)
+        self.sd.putBoolean("Autonomous Left", False)
+        self.sd.putBoolean("Autonomous Right", False)
+
         self.solenoid = wpi.solenoid
         self.lift = wpi.Spark(5)
         self.frontLeftMotor = wpi.Spark(2)
@@ -60,6 +68,7 @@ class MyRobot(wpi.IterativeRobot):
         self.timer.start()
     def autonomousPeriod(self):
         #left side left switch boi
+     if self.auto_state == 1:
         if self.timer.get() < 1.000:
             self.drive.tankDrive(.2,.2)
         elif self.timer.get() < 3.000:
@@ -67,6 +76,7 @@ class MyRobot(wpi.IterativeRobot):
         else:
             self.drive.tankDrive(0, 0)
         # right side left switch
+     elif self.auto_state == 2:
         if self.timer.get() < 1.00:
              self.drive.tankDrive(.2,.2)
         elif self.timer.get() < 3.000:
@@ -76,7 +86,8 @@ class MyRobot(wpi.IterativeRobot):
         else:
             self.drive.tankDrive(0, 0)
         # center pos left switch boi
-        if self.timer.get() < 1.000:
+     elif self.auto_state == 3:
+           if self.timer.get() < 1.000:
             self.drive.tankDrive(0,.4)
         elif self.timer.get() < 3.00
             self.drive.tankDrive(.1,.3)
@@ -84,7 +95,8 @@ class MyRobot(wpi.IterativeRobot):
             self.drive.tankDrive(.3,.0)
         else:
             self.drive.tankDrive(0, 0)
-        #right pos right switch
+       # for left switch right  pos
+       elif self.auto_state == 4:
         if self.timer.get() < 1.000:
             self.drive.tankDrive(.2,.2)
         elif self.timer.get() < 3.000:
@@ -92,6 +104,7 @@ class MyRobot(wpi.IterativeRobot):
         else:
             self.drive.tankDrive(0, 0)
         # left pos right switch
+        elif self.auto_state == 5:
         if self.timer.get() < 1.00:
             self.drive.tankDrive(.2,.2)
         elif self.timer.get() < 3.000:
@@ -101,6 +114,7 @@ class MyRobot(wpi.IterativeRobot):
         else:
             self.drive.tankDrive(0, 0)
         #center pos right switch
+        elif self.auto_state == 6:
         if self.timer.get() < 1.000:
          self.drive.tankDrive(.3,.3)
         elif self.timer.get() < 3.00
