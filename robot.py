dc7ea86ea5e4385da2a32c52e32eeb7ff51e1418
Brownman371 <castenada14@gmail.com>
2018-03-17 11:01:43 -0400

    autonmous motor tests

--- robot.py
+++ robot.py
@@ -143,7 +143,7 @@ class MyRobot(wpi.IterativeRobot):
                     self.solenoid.set(True)
                 elif self.timer.get() < 5.25:
                     self.drive.tankDrive(0,.4)
-                    #pugs indeed
+                    #pugs indeed lolz
 
 
 
